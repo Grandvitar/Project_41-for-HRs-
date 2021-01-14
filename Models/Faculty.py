714a0bf23f27c61d4081133f9c7ef8c07cefba52
Maria <61450130+Akration@users.noreply.github.com>
2021-01-14 04:25:12 +0300

    Update Faculty.py

--- Models/Faculty.py
+++ Models/Faculty.py
@@ -1,16 +1,24 @@
 import Models.Univer
+
+
 class Faculty(Models.Univer.Univer):
-    def __init__(self, id, name, description, spec_obj):
+    def __init__(self, id, name, description, spec_list, univer_id):
         super().__init__(id, name, description)
-        self._spec_obj = spec_obj
+        self.__spec_list = spec_list
+        self.__univer_id = univer_id
 
     def __str__(self):
         return f'''
 ID Факультета: {self._id}
 Название: {self._name}
 Описание: {self._description}
-Специальности: {self._spec_obj}
+Специальности: {self.__spec_list}
 '''
 
+    @property
+    def spec_list(self):
+        return self.__spec_list
 
-
+    @property
+    def univer_id(self):
+        return self.__univer_id
