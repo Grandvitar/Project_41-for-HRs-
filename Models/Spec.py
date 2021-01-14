86dc3cffc6e41d8c76afc355a872bcf868b05705
Grandvitar <75698682+Grandvitar@users.noreply.github.com>
2021-01-14 12:41:04 +0300

    Update Spec.py

--- Models/Spec.py
+++ Models/Spec.py
@@ -1,16 +1,24 @@
 import Models.Univer
 class Spec(Models.Univer.Univer):
-    def __init__(self, id, name, description, group_obj, subjects_obj):
+    def __init__(self, id, name, description, group_lst, subjects_lst):
         super().__init__(id, name, description)
-        self._group_obj = group_obj
-        self._subjects_obj = subjects_obj
+        self._subjects_lst = subjects_lst
+        self._group_lst = group_lst
 
     def __str__(self):
         return f'''
 ID Специальности: {self._id}
 Название: {self._name}
 Описание: {self._description}
-Факультет: {self._faculty_obj}
+Группы: {self._group_lst}
+Предметы: {self._subjects_lst}
 '''
+@property
+def subjects_lst(self):
+    return subjects_lst
+
+@property
+def group_lst(self):
+    return group_lst
 
 
