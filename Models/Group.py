40f026f2f5c8625b07a3a2ac675431f77c3079ff
Maria <61450130+Akration@users.noreply.github.com>
2021-01-14 04:26:04 +0300

    Update Group.py

--- Models/Group.py
+++ Models/Group.py
@@ -1,18 +1,23 @@
 class Group:
-    def __init__(self, id, stud_obj):
-        self._id = id
-        self._stud_obj = stud_obj
+    def __init__(self, id, spec_id, stud_list):
+        self.__id = id
+        self.__spec_id = spec_id
+        self.__stud_list = stud_list
 
     def __str__(self):
         return f'''
-Номер группы: {self._id}
-Ссписок студентов: {self._stud_obj}
+Номер группы: {self.__id}
+Список студентов: {self.__stud_list}
 '''
 
     @property
     def id(self):
-        return self._id
+        return self.__id
 
     @property
-    def stud_obj(self):
-        return  self._stud_obj
+    def spec_id(self):
+        return self.__spec_id
+
+    @property
+    def stud_list(self):
+        return self.__stud_list
